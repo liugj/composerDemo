c6cd170deccd4487d8a9722d4e0dafcaf45ff99a
刘国靖 <liuguojing@gaodun.com>
2016-03-25 02:58:39 +0000

    删除部分注释

--- tests/index.php
+++ tests/index.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: kendoctor
- * Date: 14-3-19
- * Time: 上午9:40
- * To change this template use File | Settings | File Templates.
- */
-
 require("../vendor/autoload.php");
 use Course\Behavior\AuthBehavior; 
 
